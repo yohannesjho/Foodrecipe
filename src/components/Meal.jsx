eb831850d71fb133dad26ad22149c9626f058a57
yohannesjho <jhonians@outlook.com>
2024-11-08 20:42:37 +0300

    correct the api

--- src/components/Meal.jsx
+++ src/components/Meal.jsx
@@ -1,31 +1,33 @@
-import React, { useEffect, useState } from 'react'
-import MealItem from './MealItem'
-import RecipeIndex from './RecipeIndex'
+import React, { useEffect, useState } from 'react';
+import MealItem from './MealItem';
+import RecipeIndex from './RecipeIndex';
 
 const Meal = () => {
-    const [item, setItem] = useState()
-    const [show,setShow] = useState(false)
-    const [url, setUrl] = useState('https:/www.themealdb.com/api/json/v1/1/search.php?f=a')
-    const [search, setSearch] = useState('')
+    const [item, setItem] = useState();
+    const [show, setShow] = useState(false);
+    const [url, setUrl] = useState('https://www.themealdb.com/api/json/v1/1/search.php?f=a');
+    const [search, setSearch] = useState('');
+
     useEffect(() => {
-        fetch(url).then(response => response.json()).then(
-            data => { console.log(data.meals) 
-                setItem(data.meals)
-                setShow(true)
-            }
+        fetch(url)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data.meals);
+                setItem(data.meals);
+                setShow(true);
+            });
+    }, [url]);
 
-        )
-    }, [url])
+    const setIndex = (alpha) => {
+        setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`);
+    };
 
-    const setIndex = (alpha) =>{
-        setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?f=${alpha}`)
-    }
+    const searchRecipe = (e) => {
+        if (e.key === "Enter") {
+            setUrl(`https://www.themealdb.com/api/json/v1/1/search.php?s=${search}`);
+        }
+    };
 
-    const searchRecipe = (e)=>{
-       if(e.key==="Enter"){
-        setUrl(`https:/www.themealdb.com/api/json/v1/1/search.php?s=${search}`)
-       }
-    }
     return (
         <>
             <div className='main'>
@@ -34,21 +36,22 @@ const Meal = () => {
                     <h4>Search your favorite food and you will get the recipe</h4>
                 </div>
                 <div className="searchBox">
-                    <input type="text" className="search-bar" onChange={e=>setSearch(e.target.value)} onKeyPress={searchRecipe}/>
+                    <input
+                        type="text"
+                        className="search-bar"
+                        onChange={e => setSearch(e.target.value)}
+                        onKeyPress={searchRecipe}
+                    />
                 </div>
                 <div className="container">
-                  
-                     {
-                        show?  <MealItem data={item}/>:"No data to show"
-                     }
+                    {show ? <MealItem data={item} /> : "No data to show"}
                 </div>
                 <div className="indexContainer">
-                    <RecipeIndex alphaIndex={(alpha)=>setIndex(alpha)}/>
+                    <RecipeIndex alphaIndex={(alpha) => setIndex(alpha)} />
                 </div>
             </div>
         </>
+    );
+};
 
-    )
-}
-
-export default Meal
+export default Meal;
